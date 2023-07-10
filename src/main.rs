3ac35e5b4cf1f9d00c95ce05c30e6d08d7350c7e
Andy Gayton <andy@thecablelounge.com>
2023-07-10 11:23:19 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use sled::Db;
 use std::io::{self, BufRead};
+use std::path::Path;
 
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct Frame {
@@ -8,28 +9,42 @@ pub struct Frame {
     pub hash: ssri::Integrity,
 }
 
-fn main() {
-    let db: Db = sled::open("my_db").unwrap();
-    let stdin = io::stdin();
+pub struct Store {
+    db: Db,
+    cache_path: String,
+}
 
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
+impl Store {
+    pub fn new(path: &str) -> Store {
+        let db = sled::open(Path::new(path).join("index")).unwrap();
+        let cache_path = Path::new(path).join("cas").to_str().unwrap().to_string();
+        Store { db, cache_path }
+    }
 
-        // Store the content from stdin as-is in cacache
-        let h = cacache::write_hash_sync("./my-cache", line.as_bytes()).unwrap();
-        println!("hash: {}", &h);
-
-        // Store a single corresponding entry in sled
+    pub fn put(&mut self, content: &[u8]) -> Frame {
+        let h = cacache::write_hash_sync(&self.cache_path, content).unwrap();
         let frame = Frame {
             id: scru128::new(),
             hash: h,
         };
         let encoded: Vec<u8> = bincode::serialize(&frame).unwrap();
-        db.insert(frame.id.to_string(), encoded).unwrap();
+        self.db.insert(frame.id.to_string(), encoded).unwrap();
+        frame
+    }
+}
+
+fn main() {
+    let mut store = Store::new("my_db");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let frame = store.put(line.as_bytes());
+        println!("Stored frame: {:?}", frame);
     }
 
     // Read and print all records from sled
-    for record in db.iter() {
+    for record in store.db.iter() {
         let record = record.unwrap();
         let decoded: Frame = bincode::deserialize(&record.1).unwrap();
         println!("{:?}", decoded);
